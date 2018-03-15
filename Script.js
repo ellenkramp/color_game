6e0704c3b199d1095c8e3bcb766e839209ca2c6e
Ellen Kramp <ellenkramp@gmail.com>
2018-03-15 12:48:52 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,10 +1,3 @@
-echo "# color_game" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/ellenkramp/color_game.git
-git push -u origin master
-
 var numSquares = 6;
 var colors = [];
 var pickedColor;
